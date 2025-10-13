f6fe5d2958a1db2609756a1a62d5fe7acbc670a8
frznprograms <shanevbharathan@gmail.com>
2025-10-13 15:29:19 +0800

    fix: corrected data preparation logic in Text8Dataset
    - create vocab and inverse vocabulary for easier debugging
    - proper use of __len__, other information can be obtained by calling
    .info()
    - fixed access of X and y in __getitem__ by accessing self.encoded
    instead of self.tokens, which is just raw tokens instead of word ids

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -4,12 +4,17 @@ import torch
 
 class Text8Dataset(Dataset):
     def __init__(self, text: str, context_size: int = 5):
-        self.tokens = [token for token in text.split()]  # word-level tokenization
+        self.tokens = text.split()  # word-level tokenization
         # NOTE: vocab doubles as word ids
-        self.vocab = {word: idx for idx, word in enumerate(set(self.tokens))}
+        self.vocab = {word: idx for idx, word in enumerate(sorted(set(self.tokens)))}
+        self.inv_vocab = {idx: word for word, idx in self.vocab.items()}
+        self.vocab_size = len(self.vocab)
         self.context_size = context_size
 
-    def __len__(self) -> dict[str, int]:
+        self.encoded = [self.vocab[word] for word in self.tokens]
+
+    @property
+    def info(self) -> dict[str, int]:
         len_properties = {
             "vocab_size": len(self.vocab),
             "context_size": self.context_size,
@@ -18,8 +23,12 @@ class Text8Dataset(Dataset):
 
         return len_properties
 
-    def __getitem__(self, idx: int) -> tuple(torch.Tensor, torch.Tensor):
-        X = self.tokens[idx : idx + self.context_size]
-        y = self.tokens[idx + self.context_size]
+    def __len__(self) -> int:
+        # number of valid (context, next_token) pairs
+        return len(self.encoded) - self.context_size
 
-        return torch.tensor(X), torch.tensor(y)
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        X = self.encoded[idx : idx + self.context_size]
+        y = self.encoded[idx + self.context_size]
+
+        return torch.tensor(X, dtype=torch.long), torch.tensor(y, dtype=torch.long)
