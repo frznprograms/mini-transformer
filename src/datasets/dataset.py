13588552716f45dd93a5610007e2f7c2e41be6af
frznprograms <shanevbharathan@gmail.com>
2025-10-14 22:27:48 +0800

    refactor: dataset.py converted to handle next-character prediction
    - changed sliding window approach to be sequence offset by 1

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -2,33 +2,22 @@ from torch.utils.data import Dataset
 import torch
 
 
-class Text8Dataset(Dataset):
+class CharDataset(Dataset):
     def __init__(self, text: str, context_size: int = 5):
-        self.tokens = text.split()  # word-level tokenization
-        # NOTE: vocab doubles as word ids
-        self.vocab = {word: idx for idx, word in enumerate(sorted(set(self.tokens)))}
-        self.inv_vocab = {idx: word for word, idx in self.vocab.items()}
-        self.vocab_size = len(self.vocab)
+        vocab = list("abcdefghijklmnopqrstuvwxyz ")
+        self.stoi = {ch: i for i, ch in enumerate(vocab)}
+        self.itos = {i: ch for ch, i in self.stoi.items()}
         self.context_size = context_size
 
-        self.encoded = [self.vocab[word] for word in self.tokens]
-
-    @property
-    def info(self) -> dict[str, int]:
-        len_properties = {
-            "vocab_size": len(self.vocab),
-            "context_size": self.context_size,
-            "num_windows": len(self.vocab) // self.context_size,
-        }
-
-        return len_properties
+        self.encoded = torch.tensor([self.stoi[ch] for ch in text], dtype=torch.long)
 
     def __len__(self) -> int:
         # number of valid (context, next_token) pairs
         return len(self.encoded) - self.context_size
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        # create non-overlapping sequences
         X = self.encoded[idx : idx + self.context_size]
-        y = self.encoded[idx + self.context_size]
+        y = self.encoded[idx + 1 : idx + 1 + self.context_size]
 
-        return torch.tensor(X, dtype=torch.long), torch.tensor(y, dtype=torch.long)
+        return X, y
