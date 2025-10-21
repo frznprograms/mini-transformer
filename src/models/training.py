c20faf56e970cb4b2e723b2c4a4e559b2a606ccb
frznprograms <shanevbharathan@gmail.com>
2025-10-21 21:33:16 +0800

    feat: added model checkpoint saving to training loop
    - saves model checkpoint at intervals defined in training configuration
    - saves final model checkpoint at the end of training

--- src/models/training.py
+++ src/models/training.py
@@ -11,13 +11,19 @@ from src.datasets.dataset import CharDataset
 from src.models.model import MiniTransformer
 from src.utils.helpers import set_device
 
+from pathlib import Path
+
+# TODO: find out where to set seed
+# TODO: plot loss history
+# TODO: implement early stopping
+
 
 @dataclass
 class ModelTrainer:
     config_path: str = "src/configs/base_configs.yaml"
     device: str = "auto"
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         configs = None
         try:
             with open(self.config_path, "r") as f:
@@ -29,6 +35,8 @@ class ModelTrainer:
             raise e
 
         self.device = set_device(self.device)
+        logger.info(f"Device has been set to {self.device}")
+
         self.model_configs = configs["model"]
         self.train_configs = configs["train"]
         self.model = MiniTransformer(**self.model_configs).to(self.device)
@@ -40,37 +48,97 @@ class ModelTrainer:
             "Model, Optimizer, Loss, and Configurations loaded successfully."
         )
 
-    def train(self, dataset: CharDataset):
-        # set model to training mode
+    @logger.catch(message="Unable to complete model training.", reraise=True)
+    def train(self, dataset: CharDataset) -> None:
         self.model.train()
         num_epochs = self.train_configs["epochs"]
         batch_size = self.train_configs["batch_size"]
 
-        n = len(dataset)
-        num_steps = num_epochs * (n // batch_size)
+        dataloader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=True, drop_last=True
+        )
+        n = len(dataloader)
 
-        logger.info("Starting Training...")
-        pbar = tqdm(num_steps)
+        logger.info(f"Starting training for {num_epochs} epochs...")
+
+        step = 0
         for epoch in range(num_epochs):
             total_loss = 0.0
 
-            for x, y in DataLoader(dataset, batch_size=batch_size, shuffle=True):
+            progress_bar = tqdm(
+                dataloader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False
+            )
+
+            for x, y in progress_bar:
                 x, y = x.to(self.device), y.to(self.device)
 
-                # forward
                 logits = self.model(x)
                 B, T, C = logits.shape
-
                 loss = self.criterion(logits.view(B * T, C), y.view(B * T))
+
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
                 total_loss += loss.item()
-                pbar.update(1)
+                progress_bar.set_postfix(loss=loss.item())
+                step += 1
+
+                self._save_model_checkpoint(step=step, pbar=progress_bar)
 
             avg_loss = total_loss / n
-            print(f"Epoch: {epoch} | Loss: {avg_loss:.4f}")
+            logger.info(
+                f"Epoch [{epoch+1}/{num_epochs}] | Average Loss: {avg_loss:.4f}"
+            )
+
+        self._save_final_checkpoint()
+        logger.success("Training complete.")
+
+    def _save_model_checkpoint(self, step: int, pbar):
+        experiment_name = self.train_configs.get(
+            "experiment_name", "unnamed_experiment"
+        )
+        checkpoint_dir = Path(f"checkpoints/{experiment_name}")
+        checkpoint_dir.mkdir(parents=True, exist_ok=True)
+
+        save_strategy = self.train_configs.get("save_strategy", None)
+        save_steps = self.train_configs.get("save_steps", None)
+        should_save_checkpoint = (
+            save_strategy == "steps"
+            and isinstance(save_steps, int)
+            and step % save_steps == 0
+        )
+
+        if should_save_checkpoint:
+            try:
+                # create specific checkpoint folder
+                step_dir = checkpoint_dir / f"checkpoint-{step}"
+                step_dir.mkdir(parents=True, exist_ok=True)
+                model_file = step_dir / "model.pt"
+
+                torch.save(self.model.state_dict(), model_file)
+                pbar.set_postfix_str(f"Saved at step {step}")
+
+            except Exception as e:
+                logger.warning(f"Unable to save model checkpoint at step {step}: {e}")
+
+    def _save_final_checkpoint(self):
+        experiment_name = self.train_configs.get(
+            "experiment_name", "unnamed_experiment"
+        )
+        checkpoint_dir = Path(f"checkpoints/{experiment_name}")
+        checkpoint_dir.mkdir(parents=True, exist_ok=True)
+
+        try:
+            final_dir = checkpoint_dir / "checkpoint-FINAL"
+            final_dir.mkdir(parents=True, exist_ok=True)
+            model_file = final_dir / "model.pt"
+
+            torch.save(self.model.state_dict(), model_file)
+            logger.success(f"Final model checkpoint saved at {model_file}")
+
+        except Exception as e:
+            logger.warning(f"Unable to save final model checkpoint: {e}")
 
 
 if __name__ == "__main__":
@@ -85,10 +153,10 @@ if __name__ == "__main__":
     with open("data/text8", "r") as f:
         full_text = f.read()
 
-    max_size = 1000
+    max_size = 10000
     data = full_text[:max_size]
 
     cd = CharDataset(text=data, context_size=10)
-    mt = ModelTrainer()
+    mt = ModelTrainer(device="cpu")
 
     mt.train(dataset=cd)
