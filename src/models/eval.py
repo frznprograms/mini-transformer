1039a434b5b13488bc81d05f921c8f6b8f3b628e
frznprograms <shanevbharathan@gmail.com>
2025-10-22 17:19:30 +0800

    feat: added more versatility to allow evaluator to be called without model checkpoint path
    - allows evaluator's eval() method to be called in trainer
    - thus no need to rewrite code for validation loop

--- src/models/eval.py
+++ src/models/eval.py
@@ -9,6 +9,7 @@ from src.models.model import MiniTransformer
 from src.utils.helpers import set_device
 
 from torch.utils.data import DataLoader
+from typing import Optional
 
 
 # TODO: find out where to set seed
@@ -16,9 +17,13 @@ from torch.utils.data import DataLoader
 
 class ModelEvaluator:
     def __init__(
-        self, model_checkpoint_path: str, model_config_path: str, device: str = "auto"
+        self,
+        model_checkpoint_path: Optional[str],
+        model_config_path: str,
+        device: str = "auto",
     ):
         configs = None
+        self.model = None
         try:
             with open(model_config_path, "r") as f:
                 configs = yaml.safe_load(f)
@@ -33,14 +38,17 @@ class ModelEvaluator:
 
         self.model_configs = configs["model"]
         self.train_configs = configs["train"]
-        self.model = MiniTransformer(**self.model_configs).to(self.device)
-        self.model.load_state_dict(torch.load(model_checkpoint_path, weights_only=True))
-        if self.model:
+        if model_checkpoint_path is not None:
+            self.model = MiniTransformer(**self.model_configs).to(self.device)
+            self.model.load_state_dict(
+                torch.load(model_checkpoint_path, weights_only=True)
+            )
+        if self.model is not None:
             logger.success("Loaded model from checkpoint and configuration.")
 
     @torch.no_grad()
-    def eval(self, dataset: CharDataset) -> dict[str, float]:
-        self.model.eval()
+    def eval(self, dataset: CharDataset) -> tuple[float, float]:
+        self.model.eval()  # type: ignore
         batch_size = self.train_configs["batch_size"]
         dataloader = DataLoader(
             dataset, batch_size=batch_size, shuffle=False, drop_last=False
@@ -56,7 +64,7 @@ class ModelEvaluator:
         progress_bar = tqdm(dataloader, desc="Evaluating...", leave=False)
         for x, y in progress_bar:
             x, y = x.to(self.device), y.to(self.device)
-            logits = self.model(x)
+            logits = self.model(x)  # type: ignore
             B, T, C = logits.shape
 
             # compute loss
@@ -77,15 +85,15 @@ class ModelEvaluator:
         logger.success(
             f"Evaluation complete. Accuracy: {accuracy:.4f}, Avg Loss: {avg_loss:.4f}"
         )
-        return {"accuracy": accuracy, "avg_loss": avg_loss}
+        return avg_loss, accuracy
 
 
 if __name__ == "__main__":
     with open("data/text8", "r") as f:
         full_text = f.read()
 
-    max_size = 1000
-    data = full_text[max_size : max_size + 100]
+    max_size = 50000
+    data = full_text[max_size : max_size + 10000]
 
     cd = CharDataset(text=data, context_size=10)
     me = ModelEvaluator(
